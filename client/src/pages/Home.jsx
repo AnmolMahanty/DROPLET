f9ac79135f6bc4c1c4cd3a80b2ded84c986db2c7
sarathak1 <sarthak2.tiwad@gmail.com>
2024-12-03 18:55:54 +0530

    backend 1

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
+import axios from "axios";
 
 const Home = () => {
+  const [message, setMessage] = React.useState("");
+  useEffect(() => {
+    axios.get('http://localhost:5000/dwf/message')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => { console.log(error) });
+  }, []);
   return (
     <>
       <Navbar />
+      <p>{message}</p>
       <div className="flex items-center justify-center w-full min-h-[calc(100vh-64px)]"></div>
     </>
   );
